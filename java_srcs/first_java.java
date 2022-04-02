a2f737bb0e3b28b4e0a568671e6a3d494cf0cf15
Abhinav Singh Bhardwaj <95396382+ab22032002@users.noreply.github.com>
2022-04-02 14:14:52 +0530

    this is the first commit for java_srcs

---
+++ java_srcs/first_java.java
@@ -0,0 +1,14 @@
+package java_srcs;
+
+public class first_java {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("hello world");
+        System.out.println("this is addition menu");
+        int num1= 34;
+        int num2= 245;
+        int num3=345;
+        int sum = num1+num2+num3;
+        System.out.println("sum = "+ sum );
+    }
+}
